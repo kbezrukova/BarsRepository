293bdcaa0360e92f79ed519bb8e3798333c7e74f
kbezrukova <95983771+kbezrukova@users.noreply.github.com>
2022-03-24 13:47:40 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace BarsTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ex = new Example();
+            ex.OnKeyPressed += Print;
+            ex.Run();
+
+            void Print(object sender, char x)
+            {
+                Console.WriteLine("\n Your symbol: " + x);
+            }
+        }
+    }
+
+    public class Example
+    {
+        public event EventHandler<char> OnKeyPressed;
+
+        public void Run()
+        {
+            while (true)
+            {
+                var x = Console.ReadKey().KeyChar;
+                if(x !='c')
+                {
+                    OnKeyPressed?.Invoke(this, x);
+                }
+                else { break; }
+            }
+        }
+    }
+}
